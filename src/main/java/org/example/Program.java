505b3ba49a847e1b746cd215fa6fdfcdc8380da1
Chandra Prakash Joshi <chandraprakashjoshi@gmail.com>
2023-08-14 22:29:45 +0530

    track runnning time

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -6,6 +6,9 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
+/**
+ * to run this: java -cp .\route-planning.main.jar org.example.Program
+ */
 public class Program {
     /**
      * Main method.
@@ -13,9 +16,12 @@ public class Program {
      */
     public static void main(final String[] args) {
         try {
+            long startTime = System.currentTimeMillis();
             RoadNetwork rn = new RoadNetwork();
             rn.readFromOsmFile("file:/C:/Users/chjos/Downloads/saarland/saarland.osm");
+            long timeTaken = System.currentTimeMillis() - startTime;
             System.out.println(rn.toString());
+            System.out.printf("TimeTaken: %d\n", timeTaken);
         } catch (ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
         }
