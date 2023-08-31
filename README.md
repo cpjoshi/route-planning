d8502c52758c67fc6f600e4bacfdecc2e4d8aadb
Chandra Prakash Joshi <chandraprakashjoshi@gmail.com>
2023-08-31 18:18:51 +0530

    query time results

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ## route-planning
 excercises from course: https://ad-wiki.informatik.uni-freiburg.de/teaching/EfficientRoutePlanningSS2012
+
+### excecrise-2
+Avg Travel Time: 34m:21s | Avg Settled Nodes: 122,241 | Avg Query Time: 33ms
+
+Avg Travel Time: 96m:9s | Avg Settled Nodes: 1,100,676 | Avg Query Time: 399ms
+
+
--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -18,38 +18,57 @@ public class Program {
      */
     public static void main(final String[] args) {
         try {
-            long startTime = System.currentTimeMillis();
-            RoadNetwork rn = new RoadNetwork();
-            rn.readFromOsmFile("file:/C:/Users/chjos/Downloads/saarland/saarland.osm");
-            System.out.println("Full graph:" + rn.toString());
-            System.out.printf("TimeTaken: %d\n", System.currentTimeMillis() - startTime);
+            String fileUrl = "file:/C:/Users/chjos/Downloads/saarland/saarland.osm";
+            System.out.println("Analyzing saarland...");
+            osmDataAnalysis(fileUrl);
 
-            rn.reduceToLargestConnectedComponent();
-            System.out.println("Largest connected component: "+ rn.toString());
-
-            DijkstrasAlgorithm dijkstrasAlgorithm = new DijkstrasAlgorithm(rn);
-            System.out.printf("Time-to-drive, queryTime, SettledNodes\n");
-            long totalTime = 0;
-            long totalSettledNodes = 0;
-            for(int i=0; i<100; i++) {
-                Random random = new Random();
-                int source = random.nextInt(rn.getNodesCount());
-                int dest = random.nextInt(rn.getNodesCount());
-                startTime = System.currentTimeMillis();
-                int dist = dijkstrasAlgorithm.computeShortestPath(source, dest);
-                totalTime += dist;
-                totalSettledNodes += dijkstrasAlgorithm.getSettledNodes();
-                System.out.printf("%s, %d, %d\n",
-                        String.format("%dm:%ds", dist/60, dist%60),
-                        dijkstrasAlgorithm.getSettledNodes(),
-                        System.currentTimeMillis() - startTime);
-            }
-            totalTime = totalTime/100;
-            System.out.printf("Avg Travel Time: %s, Avg Settled Nodes: %d\n", String.format("%dm:%ds", totalTime/60, totalTime%60),
-                    totalSettledNodes/100);
+            fileUrl = "file:/C:/Users/chjos/Downloads/baden-wuerttemberg/baden-wuerttemberg.osm";
+            System.out.println("Analyzing baden-wuerttemberg...");
+            osmDataAnalysis(fileUrl);
         } catch (ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
         }
         System.out.println("Done!");
+    }
+
+    private static void osmDataAnalysis(String fileUrl)
+            throws ParserConfigurationException, SAXException, IOException {
+        RoadNetwork rn = new RoadNetwork();
+        long startTime = System.currentTimeMillis();
+        rn.readFromOsmFile(fileUrl);
+        System.out.println("Full graph:" + rn.toString());
+        System.out.printf("TimeTaken: %d\n", System.currentTimeMillis() -
+                startTime);
+
+        rn.reduceToLargestConnectedComponent();
+        System.out.println("Largest connected component: "+ rn.toString());
+
+        DijkstrasAlgorithm dijkstrasAlgorithm = new DijkstrasAlgorithm(rn);
+        System.out.printf("Time-to-drive, queryTime, SettledNodes\n");
+        long totalTime = 0;
+        long totalSettledNodes = 0;
+        long totalQueryTime = 0;
+
+        for(int i=0; i<100; i++) {
+            Random random = new Random();
+            int source = random.nextInt(rn.getNodesCount());
+            int dest = random.nextInt(rn.getNodesCount());
+            startTime = System.currentTimeMillis();
+            int dist = dijkstrasAlgorithm.computeShortestPath(source, dest);
+            long queryTime = System.currentTimeMillis() - startTime;
+            totalTime += dist;
+            totalSettledNodes += dijkstrasAlgorithm.getSettledNodes();
+            totalQueryTime += queryTime;
+
+            System.out.printf("%s, %d, %d\n",
+                    String.format("%dm:%ds", dist/60, dist%60),
+                    dijkstrasAlgorithm.getSettledNodes(),
+                    queryTime);
+        }
+        totalTime = totalTime/100;
+        System.out.printf("Avg Travel Time: %s, Avg Settled Nodes: %d, Avg Query Time: %d\n",
+                String.format("%dm:%ds", totalTime/60, totalTime%60),
+                totalSettledNodes/100,
+                totalQueryTime/100);
     }
 }
